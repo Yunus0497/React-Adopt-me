a1ec73c2da31f95e91a33d2190b055c2c9a4a156
Yunus0497 <Mohammed@fixnix.co>
2020-12-31 07:18:45 +0530

    Converted Pure React code to Jsx

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", type: "Dog", breed: "Havanese" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      type: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", type: "Cat", breed: "Mixed" })
+  return (
+    <div>
+      <h1> Adopt Me!</h1>
+      <Pet name="Luna" type="Dog" breed="Havanese" />
+      <Pet name="Pepper" type="Bird" breed="Cockatiel" />
+      <Pet name="Doink" type="Cat" breed="Mixed" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
